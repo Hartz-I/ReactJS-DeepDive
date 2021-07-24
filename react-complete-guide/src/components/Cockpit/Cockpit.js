205134d3b3cec26d47c04a5958ab52443de7ce46
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2021-07-24 22:36:32 +0600

    added title style

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -45,7 +45,7 @@ const cockpit = (props) => {
 
   return (
     <div>
-      <h1>Hi, I'm a react app!</h1>
+      <h1 className="title">Hi, I'm a react app!</h1>
       <p className={classes.join(" ") /**this joins the classes with space */}>
         This is really working!!
       </p>
